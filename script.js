5f305d896ae10438fb2a96c23fc4eec6f7a956de
ronak127 <131310388+ronak127@users.noreply.github.com>
2023-04-19 19:14:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,79 @@
-const loginForm = document.getElementById('loginForm');
-const gasPriceForm = document.getElementById('gasPriceForm');
-
-function saveGasPrices(regular, midGrade, premium, diesel) {
-  localStorage.setItem('gasPrices', JSON.stringify({ regular, midGrade, premium, diesel }));
-}
-
-function loadGasPrices() {
-  const gasPrices = JSON.parse(localStorage.getItem('gasPrices'));
-  if (gasPrices) {
-    document.getElementById('regular').value = gasPrices.regular;
-    document.getElementById('midGrade').value = gasPrices.midGrade;
-    document.getElementById('premium').value = gasPrices.premium;
-    document.getElementById('diesel').value = gasPrices.diesel;
+// Add your Firebase configuration here
+const firebaseConfig = {
+    apiKey: "AIzaSyA2FTLG_RgzXUOYz5UZ3CISuiw5bDHRIuw",
+    authDomain: "gas-prices-4ff65.firebaseapp.com",
+    projectId: "gas-prices-4ff65",
+    storageBucket: "gas-prices-4ff65.appspot.com",
+    messagingSenderId: "334309115103",
+    appId: "G-W93C9PP94F"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  // Get elements
+  const loginForm = document.getElementById('loginForm');
+  const gasPriceForm = document.getElementById('gasPriceForm');
+  
+  // Add login event
+  if (loginForm) {
+    loginForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+  
+      // Get email and password
+      const email = document.getElementById('email').value;
+      const password = document.getElementById('password').value;
+  
+      // Sign in
+      firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(() => {
+          window.location.replace("index.html");
+        })
+        .catch((error) => {
+          console.error("Error signing in:", error);
+        });
+    });
   }
-}
-
-if (loginForm) {
-  loginForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-
-    // Simple username and password check (not secure)
-    if (username === 'admin' && password === 'password') {
-      location.href = 'index.html';
+  
+  // Add gas price form event
+  if (gasPriceForm) {
+    gasPriceForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+  
+      // Get gas prices
+      const regular = document.getElementById('regular').value;
+      const midGrade = document.getElementById('midGrade').value;
+      const premium = document.getElementById('premium').value;
+      const diesel = document.getElementById('diesel').value;
+  
+      // Save gas prices to Firestore
+      const db = firebase.firestore();
+      db.collection("gasPrices").add({
+        regular: parseFloat(regular),
+        midGrade: parseFloat(midGrade),
+        premium: parseFloat(premium),
+        diesel: parseFloat(diesel),
+        timestamp: firebase.firestore.FieldValue.serverTimestamp()
+      })
+      .then(() => {
+        console.log("Gas prices saved successfully.");
+        gasPriceForm.reset();
+      })
+      .catch((error) => {
+        console.error("Error saving gas prices:", error);
+      });
+    });
+  }
+  
+  // Realtime listener
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      console.log("User is signed in.");
     } else {
-      alert('Invalid username or password');
+      console.log("No user is signed in.");
+      if (window.location.pathname !== '/login.html') {
+        window.location.replace("login.html");
+      }
     }
   });
-}
-
-if (gasPriceForm) {
-  loadGasPrices();
-
-  gasPriceForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-
-    const regular = document.getElementById('regular').value;
-    const midGrade = document.getElementById('midGrade').value;
-    const premium = document.getElementById('premium').value;
-    const diesel = document.getElementById('diesel').value;
-
-    saveGasPrices(regular, midGrade, premium, diesel);
-    alert('Gas prices saved successfully');
-  });
-}
+  
