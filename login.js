96add0fe1ea433ad5725fd728df132971d1cdde6
ronak127 <131310388+ronak127@users.noreply.github.com>
2023-04-20 16:45:07 -0400

    Update login.js

--- login.js
+++ login.js
@@ -1,46 +1,24 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyA2FTLG_RgzXUOYz5UZ3CISuiw5bDHRIuw",
-  authDomain: "gas-prices-4ff65.firebaseapp.com",
-  projectId: "gas-prices-4ff65",
-  storageBucket: "gas-prices-4ff65.appspot.com",
-  messagingSenderId: "334309115103",
-  appId: "G-W93C9PP94F"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-document.getElementById("loginForm").addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-
-  console.log("Trying to log in with email:", email);
-
-  firebase
-    .auth()
-    .signInWithEmailAndPassword(email, password)
-    .then((userCredential) => {
-      // Signed in
-      const user = userCredential.user;
-      console.log("Logged in successfully with user:", user);
-      window.location.href = "index.html";
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      console.log("Error code:", errorCode);
-      console.log("Error message:", errorMessage);
-      alert("Error: " + errorMessage);
-    });
-});
-
-firebase.auth().onAuthStateChanged((user) => {
-  if (user) {
-    console.log("User is logged in:", user);
-    window.location.href = "index.html";
-  } else {
-    console.log("User is not logged in");
-  }
-});
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles.css">
+  <title>Login</title>
+  <script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js"></script>
+  <script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-auth.js"></script>
+</head>
+<body>
+  <h1>Login</h1>
+  <form id="loginForm">
+    <label for="email">Email:</label>
+    <input type="email" id="email" required>
+    <br><br>
+    <label for="password">Password:</label>
+    <input type="password" id="password" required>
+    <br><br>
+    <button type="submit">Login</button>
+  </form>
+  <script src="login.js"></script>
+</body>
+</html>
