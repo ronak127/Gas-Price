cdc879bdb8c19a972ca1f535a9a4219f654510b4
ronak127 <131310388+ronak127@users.noreply.github.com>
2023-04-19 23:21:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,36 @@
-// Your web app's Firebase configuration
+// Replace with your Firebase project configuration values
 const firebaseConfig = {
   apiKey: "AIzaSyA2FTLG_RgzXUOYz5UZ3CISuiw5bDHRIuw",
   authDomain: "gas-prices-4ff65.firebaseapp.com",
   projectId: "gas-prices-4ff65",
   storageBucket: "gas-prices-4ff65.appspot.com",
   messagingSenderId: "334309115103",
-  appId: "G-W93C9PP94F"
+  appId: "G-W93C9PP94F",
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-window.onload = () => {
-  const loginForm = document.getElementById("loginForm");
+const loginForm = document.getElementById('loginForm');
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
 
-  loginForm.addEventListener("submit", (event) => {
-    event.preventDefault();
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
 
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
+  const email = emailInput.value;
+  const password = passwordInput.value;
 
-    firebase
-      .auth()
-      .signInWithEmailAndPassword(username, password)
-      .then((userCredential) => {
-        // Signed in
-        window.location.href = "index.html";
-      })
-      .catch((error) => {
-        console.error("Error signing in:", error);
-        alert("Login failed. Please check your credentials.");
-      });
-  });
-};
+  // Sign in with email and password
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      window.location.href = 'index.html'; // Redirect to the main page
+    })
+    .catch((error) => {
+      // Handle errors
+      console.error('Error signing in:', error);
+      alert('Error signing in. Please check your email and password.');
+    });
+});
