e6b09b1eb46e9e82cef8779b8e35a6d82c5d9556
ronak127 <131310388+ronak127@users.noreply.github.com>
2023-04-20 00:24:08 -0400

    Update login.js

--- login.js
+++ login.js
@@ -16,17 +16,22 @@ document.getElementById("loginForm").addEventListener("submit", (e) => {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
+  console.log("Trying to log in with email:", email); // Add this line
+
   firebase
     .auth()
     .signInWithEmailAndPassword(email, password)
     .then((userCredential) => {
       // Signed in
       const user = userCredential.user;
+      console.log("Logged in successfully with user:", user); // Add this line
       window.location.href = "index.html";
     })
     .catch((error) => {
       const errorCode = error.code;
       const errorMessage = error.message;
+      console.log("Error code:", errorCode); // Add this line
+      console.log("Error message:", errorMessage); // Add this line
       alert("Error: " + errorMessage);
     });
 });
