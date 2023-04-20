c4e782ff85ec4c172f78942850f80cc86d388ed5
ronak127 <131310388+ronak127@users.noreply.github.com>
2023-04-20 16:50:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,72 +1,75 @@
-// Replace these with your own configuration values
 const firebaseConfig = {
   apiKey: "AIzaSyA2FTLG_RgzXUOYz5UZ3CISuiw5bDHRIuw",
   authDomain: "gas-prices-4ff65.firebaseapp.com",
   projectId: "gas-prices-4ff65",
   storageBucket: "gas-prices-4ff65.appspot.com",
   messagingSenderId: "334309115103",
-  appId: "G-W93C9PP94F",
+  appId: "G-W93C9PP94F"
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
+// Initialize Firestore
 const db = firebase.firestore();
 
-document.getElementById("gasPricesForm").addEventListener("submit", (e) => {
-  e.preventDefault();
+function signOut() {
+  firebase.auth().signOut().then(() => {
+    window.location.href = "login.html";
+  }).catch((error) => {
+    console.error("Error signing out:", error);
+  });
+}
 
-  const regularPrice = document.getElementById("regular").value;
-  const midgradePrice = document.getElementById("midgrade").value;
-  const premiumPrice = document.getElementById("premium").value;
-  const dieselPrice = document.getElementById("diesel").value;
+function updatePrice(id, value) {
+  const priceElement = document.getElementById(id);
+  priceElement.textContent = value ? `$${value.toFixed(3)}` : "-";
+}
 
-  const priceUpdates = {};
-
-  if (regularPrice) {
-    priceUpdates.regular = regularPrice;
-  }
-  if (midgradePrice) {
-    priceUpdates.midgrade = midgradePrice;
-  }
-  if (premiumPrice) {
-    priceUpdates.premium = premiumPrice;
-  }
-  if (dieselPrice) {
-    priceUpdates.diesel = dieselPrice;
-  }
-
-  db.collection("gasPrices").doc("currentPrices").update(priceUpdates);
-
-  if (regularPrice) {
-    document.getElementById("regularPrice").textContent = `$ ${regularPrice}9`;
-  }
-  if (midgradePrice) {
-    document.getElementById("midgradePrice").textContent = `$ ${midgradePrice}9`;
-  }
-  if (premiumPrice) {
-    document.getElementById("premiumPrice").textContent = `$ ${premiumPrice}9`;
-  }
-  if (dieselPrice) {
-    document.getElementById("dieselPrice").textContent = `$ ${dieselPrice}9`;
-  }
-});
-
-// Fetch data from Firestore when the page loads and update the respective elements
-db.collection("gasPrices")
-  .doc("currentPrices")
-  .get()
-  .then((doc) => {
+function loadPrices() {
+  db.collection("gasPrices").doc("currentPrices").get().then((doc) => {
     if (doc.exists) {
       const data = doc.data();
-      document.getElementById("regularPrice").textContent = `$ ${data.regular}9`;
-      document.getElementById("midgradePrice").textContent = `$ ${data.midgrade}9`;
-      document.getElementById("premiumPrice").textContent = `$ ${data.premium}9`;
-      document.getElementById("dieselPrice").textContent = `$ ${data.diesel}9`;
+      updatePrice("regularPrice", data.regular);
+      updatePrice("midgradePrice", data.midgrade);
+      updatePrice("premiumPrice", data.premium);
+      updatePrice("dieselPrice", data.diesel);
     } else {
-      console.log("No such document!");
+      console.error("No document found with ID 'currentPrices'");
     }
-  })
-  .catch((error) => {
-    console.log("Error getting document:", error);
+  }).catch((error) => {
+    console.error("Error fetching prices:", error);
   });
+}
+
+document.getElementById("priceForm").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const regular = parseFloat(document.getElementById("regular").value) || null;
+  const midgrade = parseFloat(document.getElementById("midgrade").value) || null;
+  const premium = parseFloat(document.getElementById("premium").value) || null;
+  const diesel = parseFloat(document.getElementById("diesel").value) || null;
+
+  const updatedPrices = {
+    regular: regular !== null ? regular : firebase.firestore.FieldValue.delete(),
+    midgrade: midgrade !== null ? midgrade : firebase.firestore.FieldValue.delete(),
+    premium: premium !== null ? premium : firebase.firestore.FieldValue.delete(),
+    diesel: diesel !== null ? diesel : firebase.firestore.FieldValue.delete()
+  };
+
+  db.collection("gasPrices").doc("currentPrices").set(updatedPrices, { merge: true })
+    .then(() => {
+      loadPrices();
+    })
+    .catch((error) => {
+      console.error("Error updating prices:", error);
+    });
+});
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    loadPrices();
+  } else {
+    window.location.href = "login.html";
+  }
+});
