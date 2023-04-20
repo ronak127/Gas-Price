039acb1a16e857705968ef255ccc079c97eb5098
ronak127 <131310388+ronak127@users.noreply.github.com>
2023-04-20 00:15:10 -0400

    Create login.js

---
+++ login.js
@@ -0,0 +1,34 @@
+const firebaseConfig = {
+  // Place your Firebase configuration values here
+  const firebaseConfig = {
+    apiKey: "AIzaSyA2FTLG_RgzXUOYz5UZ3CISuiw5bDHRIuw",
+    authDomain: "gas-prices-4ff65.firebaseapp.com",
+    projectId: "gas-prices-4ff65",
+    storageBucket: "gas-prices-4ff65.appspot.com",
+    messagingSenderId: "334309115103",
+    appId: "G-W93C9PP94F"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+document.getElementById("loginForm").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      window.location.href = "index.html";
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert("Error: " + errorMessage);
+    });
+});
